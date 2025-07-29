43a95d5cac1f73a16e706b9485f7fc449c312288
rajeshchauhan1426 <rc810609@gmail.com>
2025-07-29 15:42:28 +0530

    type error fixed of the calender

--- src/app/components/ui/calender.tsx
+++ src/app/components/ui/calender.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
+        PreviousMonthButton: (props) => <button {...props}><ChevronLeft className="h-4 w-4" /></button>,
+        NextMonthButton: (props) => <button {...props}><ChevronRight className="h-4 w-4" /></button>,
       }}
       {...props}
     />
